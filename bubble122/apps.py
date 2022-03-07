77fd79e109016cdee593e77bcd51fce7a339e946
Evelyn Siu <esiu@linux3.cs.uchicago.edu>
2022-03-07 16:55:33 -0600

    pushing all new files

--- bubble122/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Bubble122Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bubble122'
--- ui/settings.py
+++ ui/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'ui.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db')
+        'NAME': os.path.join(BASE_DIR, '/bubble_tables.db')
     }
 }
 
