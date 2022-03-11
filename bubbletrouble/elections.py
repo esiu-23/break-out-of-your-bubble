f31ddf13cad9cde1fe2de9273dc4ae9a23b08e64
Carolyn Vilter <cvilter@uchicago.edu>
2022-03-11 00:42:18 -0600

    elections updates: merge, limit, swap county, calc vote %s

--- bubbletrouble/elections.py
+++ bubbletrouble/elections.py
@@ -3,18 +3,42 @@
 
 '''
 Contents:
-    # Import election data
+    # Import election, acs data
     # Join ACS county variable
     # Create/Clean vote variables
 '''
 
 import subprocess
+import pandas as pd
 
 # Pull election data from box via shell script 
 subprocess.run(['bash', 'elections.sh'])
 
-# Join cleaner ACS county var on FIPS
+# Read data as pd dfs:
+elections = pd.read_csv("data/elections.csv")
+acs = pd.read_csv("data/acs_demos.csv")
+
+# Limit elections to 2016; join with cleaner ACS county_name
+elections = elections[elections["year"] == 2016]
+elections = elections.merge(acs[["fips", "county_name"]], how="left", on="fips")
+
+# Visual scan for correct merge
+elections.head(100)
+elections.tail(100)
+
+# Clean / create vars
+elections.drop(columns = ["county"], inplace=True)
+elections.rename(columns = {"county_name": "county"}, inplace=True)
+
+elections["dvotes_pct"] = elections["dvotes"]/(elections["dvotes"] + 
+                            elections["rvotes"]) * 100
+elections["rvotes_pct"] = elections["rvotes"]/(elections["dvotes"] + 
+                            elections["rvotes"]) * 100
+
+elections = elections.round(decimals = 2)
+
+elections["county"] = elections["county"].str.strip(" County")
 
 
-# Clean and create vars
-
+# Export cleaned elections
+elections.to_csv("data/elections.csv")
