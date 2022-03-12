afe6a2896fec75437674a5e266d963fa8e95bf66
Carolyn Vilter <cvilter@uchicago.edu>
2022-03-12 13:15:37 -0600

    ui_list now outputs sorted, deduped county_list for drop-down

--- bubbletrouble/res/ui_lists.py
+++ bubbletrouble/res/ui_lists.py
@@ -7,12 +7,19 @@ def generate_lists():
     ###  Replace with name of our database
     connection = sqlite3.connect('../bubble_tables.db')
     c = connection.cursor()
-    # print(c)
 
     # get lists of unique values from sql database
     state = c.execute('''SELECT DISTINCT state FROM elections''').fetchall()
     county = c.execute('''SELECT DISTINCT county FROM elections''').fetchall()
     
+    # Making the county list (drop-down) more usable:
+    for i, _ in enumerate(county):  # de-tuple
+        county[i] = county[i][0]            
+    county = set(county)  # use set to deduplicate
+    county = list(county)  # back to list
+    county.sort()  # alphabetize
+    county.remove('county')  # remove the header
+
     connection.close()
 
 
@@ -22,15 +29,14 @@ def generate_lists():
     f = open('state_list.csv', 'w')
     w = csv.writer(f, delimiter="|")
     for i, row in enumerate(state):
-        if i!=0:
+        if i!=0:   # don't write out the header row
             w.writerow(row)
     f.close()
 
     f = open('county_list.csv', 'w')
     w = csv.writer(f, delimiter="|")
-    for i, row in enumerate(county):
-        if i!=0:
-            w.writerow(row)
+    for row in county:
+        w.writerow({row})
     f.close()
 
 generate_lists()
